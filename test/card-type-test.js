21674f362b2fff0f6bdf3f4113fa1f2297d4b684
Ugochukwu Okeke <ugochukwu.okeke@Interswitchng.com>
2017-02-12 03:58:39 +0100

    IA-6025

--- test/card-type-test.js
+++ test/card-type-test.js
@@ -22,21 +22,68 @@ describe("Card Type Checker", function(){
 
         it("matches when the card starts with a 4 for VISA ", function(){
 
-            var card = new CardType("411");
+            var card = CardType("411");
             expect(card.length).to.equal(1);
         
         });
 
         it("matches when a card starts with 5 for MasterCard ", function(){
 
-            var card = new CardType("51");
+            var card = CardType("51");
             expect(card.length).to.equal(1);
         });
 
         it("Does not match when a card starts with 1 ", function(){
 
-            var card = new CardType("1");
+            var card = CardType("1");
             expect(card.length).to.equal(0);
+        });
+    });
+
+    it("It returns an empty array if passed a non-string ", function(){
+
+        expect(CardType()).to.deep.equal([]);
+        expect(CardType(null)).to.deep.equal([]);
+        expect(CardType(true)).to.deep.equal([]);
+        expect(CardType(false)).to.deep.equal([]);
+        expect(CardType('magnum claud')).to.deep.equal([]);
+        expect(CardType(3920342)).to.deep.equal([]);
+        expect(CardType([])).to.deep.equal([]);
+        expect(CardType({})).to.deep.equal([]);
+    });
+
+    describe("matches card number to issuer ", function(){
+
+        var tests = [
+            ['4', 'visa'],
+            ['411', 'visa'],
+            ['4111111111111111', 'visa'],
+            ['4012888888881881', 'visa'],
+            ['4222222222222', 'visa'],
+            ['4462030000000000', 'visa'],
+            ['4484070000000000', 'visa'],
+            ['411111111111111111', 'visa'],
+            ['4111111111111111110', 'visa'],
+            ['51', 'master-card'],
+            ['52', 'master-card'],
+            ['53', 'master-card'],
+            ['54', 'master-card'],
+            ['55', 'master-card'],
+            ['5555555555554444', 'master-card'],
+            ['5454545454545454', 'master-card']
+        ];
+
+        tests.forEach(function(test){
+
+            var number = test[0];
+            var type = test[1];
+
+            it("should match "+type+" for this number: "+number, function(){
+
+                var card = CardType(number);
+                expect(card).to.have.lengthOf(1);
+                expect(card[0].type).to.equal(type);
+            });
         });
     });
 });
