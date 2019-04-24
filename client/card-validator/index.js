4b889afdb53bba15f321528840b69278f2ebf833
ramses-iv <ongerigiddy@gmail.com>
2019-04-24 17:20:42 +0300

    Added validator for token form field

--- client/card-validator/index.js
+++ client/card-validator/index.js
@@ -1,6 +1,7 @@
 module.exports = {
     cvv: require('./src/cvv.js'),
     pan: require('./src/pan.js'),
+    token: require('./src/token.js'),
     pin: require('./src/pin.js'),
     exp: require('./src/exp.js')
 };
